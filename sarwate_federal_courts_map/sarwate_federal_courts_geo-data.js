f13eb3779db8402a6ad1265966a43fcbcd7d6b5c
js5966 <93935138+js5966@users.noreply.github.com>
2022-01-21 19:29:45 -0800

    added files

---
+++ sarwate_federal_courts_map/sarwate_federal_courts_geo-data.js
@@ -0,0 +1,252 @@
+infoData = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Kelley,Angel","article": "<p>Judge Angel Kelley</p>", "radius":7, "color": "#FFFF00", "group_id": 1, "group_name": "MA", "headline":"Judges Confirmed to District 10 - MA"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -70.8005523679998,
+          41.2509803770001
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Merriam,Sarah A.L.","article": "<p>text</p>", "radius":7, "color": "#FFFF00", "group_id": 2, "group_name": "CT", "headline":"Sarah A. L. Merriam"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -71.797943115,
+          42.0088882450001
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Nagala,Sarala Vidya","article": "<p>text</p>", "radius":7, "color": "#FFFF00", "group_id": 2, "group_name": "CT", "headline":"Sarala Vidya Nagala"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -71.797943115,
+          42.0088882450001
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Williams,Omar Antonio","article": "<p>Judges Sarah A. L. Merriam, Sarala Vidya Nagala, and Omar Antonio Willams</p>", "radius":21, "color": "#FFFF00", "group_id": 2, "group_name": "CT", "headline":"Judges Confirmed to District 02 - CT"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -71.797943115,
+          42.0088882450001
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Neals,Julien Xavier","article": "<p>text</p>", "radius":28, "color": "#FFFF00", "group_id": 3, "group_name": "NJ", "headline":"Julien Xavier Neals"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.4418487549999,
+          39.4046859740002
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "O'Hearn,Christine P.","article": "<p>text</p>", "radius":28, "color": "#FFFF00", "group_id": 3, "group_name": "NJ", "headline":"Christine P. O'Hearn"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.4418487549999,
+          39.4046859740002
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Quraishi,Zahid N.","article": "<p>text</p>", "radius":28, "color": "#FFFF00", "group_id": 3, "group_name": "NJ", "headline":"Zahid N. Quraishi"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.4418487549999,
+          39.4046859740002
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Williams,Karen McGlashan","article": "<p>Judges Julien Xavier Neals, Christine P. O'Hearn, Zahid N. Quraishi, and Karen McGlashan Williams</p>", "radius":28, "color": "#FFFF00", "group_id": 3, "group_name": "NJ", "headline":"Judges Confirmed to District 03 - NJ"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.4418487549999,
+          39.4046859740002
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Boardman,Deborah L.","article": "<p>text</p>", "radius":14, "color": "#FFFF00", "group_id": 4, "group_name": "MD", "headline":"Deborah L. Boardman"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.9919662479999,
+          37.9564323430002
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Griggsby,Lydia Kay","article": "<p>Judges Deborah L. Boardman and Lydia Kay Griggsby</p>", "radius":14, "color": "#FFFF00", "group_id": 4, "group_name": "MD", "headline":"Judges Confirmed to District 04 - MD"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.9919662479999,
+          37.9564323430002
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Giles,Patricia Tolliver","article": "<p>text</p>", "radius":14, "color": "#FFFF00", "group_id": 5, "group_name": "VAE", "headline":"Patricia Tolliver Giles"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.9206466669999,
+          36.5646018980001
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Nachmanoff,Michael S.","article": "<p>Judges Patricia Tolliver Giles and Michael S. Nachmanoff</p>", "radius":14, "color": "#FFFF00", "group_id": 5, "group_name": "VAE", "headline":"Judges Confirmed to District 04 - VAE"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.9206466669999,
+          36.5646018980001
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Estudillo,David G.","article": "<p>text</p>", "radius":21, "color": "#FFFF00", "group_id": 6, "group_name": "WAW", "headline":"David G. Estudillo"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -123.423522949,
+          46.206237793
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "King,Lauren J.","article": "<p>text</p>", "radius":21, "color": "#FFFF00", "group_id": 6, "group_name": "WAW", "headline":"Lauren J. King"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -123.423522949,
+          46.206237793
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Lin,Tana","article": "<p>Judges David G. Estudillo, Lauren J. King, and Tana Lin</p>", "radius":21, "color": "#FFFF00", "group_id": 6, "group_name": "WAW", "headline":"Judges Confirmed to District 09 - WAW"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -123.423522949,
+          46.206237793
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Rodriguez,Regina M.","article": "<p>Judge Regina M. Rodriguez</p>", "radius":7, "color": "#FFFF00", "group_id": 7, "group_name": "CO", "headline":"Judges Confirmed to District 10 - CO"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -104.052841187,
+          41.00169754
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Strickland,Margaret Irene","article": "<p>Judge Margaret Irene Strickland</p>", "radius":7, "color": "#FFFF00", "group_id": 8, "group_name": "NM", "headline":"Judges Confirmed to District 10 - NM"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -103.000869751,
+          36.9989891050001
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Cobb,Jia M.","article": "<p>text</p>", "radius":14, "color": "#FFFF00", "group_id": 9, "group_name": "DC", "headline":"Jia M. Cobb"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.0261611939999,
+          38.801475525
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Pan,Florence Y.","article": "<p>Judges Jia M. Cobb and Florence Y. Pan</p>", "radius":14, "color": "#FFFF00", "group_id": 9, "group_name": "DC", "headline":"Judges Confirmed to District DC - DC"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.0261611939999,
+          38.801475525
+        ]
+      }
+    }
+  ]
+}
